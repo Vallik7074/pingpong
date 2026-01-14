7e384dac20ad2021fd05399e680160898e27a63c
Vallik7074 <akv4for@gmail.com>
2026-01-14 18:14:28 +0300

    + ball

--- proekt.py
+++ proekt.py
@@ -13,12 +13,33 @@ class Player():
             self.hitbox.y -= self.speed
         if key_list[pygame.K_s]:
             self.hitbox.y += self.speed
-        if self.hitbox.bottom > 750:
-            self.hitbox.bottom = 750
-        if self.hitbox.top < 50:
-            self.hitbox.top = 50
+        if self.hitbox.bottom > 775:
+            self.hitbox.bottom = 775
+        if self.hitbox.top < 25:
+            self.hitbox.top = 25
+
+class Ball():
+    def __init__ (self, x, y, speed):
+        self.hitbox = pygame.Rect(x, y, 40, 40)
+        self.speed = speed
+        self.speed_x = speed
+        self.speed_y = speed
+    def move(self):
+        self.hitbox.x += self.speed_x
+        self.hitbox.y += self.speed_y
+        if self.hitbox.top < 0:
+            self.speed_y = self.speed
+        if self.hitbox.top > 800:
+            self.speed_y = - self.speed
+        if self.hitbox.left < 0:
+            self.speed_x = self.speed
+        if self.hitbox.right > 1000:
+            self.speed_x = - self.speed
+
 
 player1 = Player(50, 200, 4)
+ball = Ball(500, 400, 5)
+
 while True:
     okno.fill((0, 0, 0))
     event_list = pygame.event.get()
@@ -26,6 +47,8 @@ while True:
         if e.type == pygame.QUIT:
             exit()
     player1.move()
+    ball.move()
     pygame.draw.rect(okno, (255, 0, 0), player1.hitbox)
+    pygame.draw.rect(okno, (255, 0, 0), ball.hitbox)
     pygame.display.update()
     clock.tick(100)
